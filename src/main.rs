3333586bd2b29c0f9b7785c84b63b947ea0aa86a
beedi <akshatkaushik9130@gmail.com>
2025-08-10 15:30:23 +0000

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,14 @@ use std::process;
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
         return input_line.contains(pattern);
+    
+    } 
+    else if pattern == "\\d" {
+        return input_line.chars().any(|c| c.is_ascii_digit());
     } else {
         panic!("Unhandled pattern: {}", pattern)
     }
+    
 }
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
